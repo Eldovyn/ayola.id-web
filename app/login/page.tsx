01cd1bb9458e6b2e8902107e801d517b4bf2b013
eldovyn <farras.pramudita@gmail.com>
2025-11-05 00:49:16 +0700

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,31 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+const PageLogin = () => {
+    return (
+        <>
+            <div className="h-screen flex justify-center items-center bg-[#EEF2F5]">
+                <div className="bg-[#FFFFFF] w-150 h-150 rounded-lg flex flex-col gap-2 justify-center">
+                    <h1 className="text-xl font-bold ps-20 pe-20">Masuk Akun untuk Pesan Lapangan</h1>
+                    <p className="ps-20 pe-20 text-gray-500 text-sm">Masuk untuk pesan lapangan, kelola jadwal, dan lihat riwayat booking</p>
+                    <form action="" className="pt-20 flex gap-5 flex-col">
+                        <div className="flex flex-col gap-2 ms-20 me-20 ">
+                            <Label htmlFor="email">Email</Label>
+                            <Input type="email" placeholder="Email" />
+                        </div>
+                        <div className="flex flex-col gap-2 ms-20 me-20 ">
+                            <Label htmlFor="password">Password</Label>
+                            <Input type="password" placeholder="Password" />
+                        </div>
+                        <Button className="w-110 mx-auto bg-blue-500 hover:bg-blue-600 cursor-pointer">Login</Button>
+                        <p className="text-sm text-end me-20">dont have an account? <span><Link href="/register" className="text-blue-500 underline">register</Link></span></p>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PageLogin
