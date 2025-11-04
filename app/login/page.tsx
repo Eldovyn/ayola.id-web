c6b232483937c572cbf1456022002e6d8c8fbaee
eldovyn <farras.pramudita@gmail.com>
2025-11-05 00:54:57 +0700

    style(login): update ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,9 +8,9 @@ const PageLogin = () => {
         <>
             <div className="h-screen flex justify-center items-center bg-[#EEF2F5]">
                 <div className="bg-[#FFFFFF] w-150 h-150 rounded-lg flex flex-col gap-2 justify-center">
-                    <h1 className="text-xl font-bold ps-20 pe-20">Masuk Akun untuk Pesan Lapangan</h1>
-                    <p className="ps-20 pe-20 text-gray-500 text-sm">Masuk untuk pesan lapangan, kelola jadwal, dan lihat riwayat booking</p>
-                    <form action="" className="pt-20 flex gap-5 flex-col">
+                    <h1 className="text-3xl font-bold text-center">Welcome Back To Chaty</h1>
+                    <p className="ps-20 pe-20 text-gray-500 text-sm text-center -mt-2">Sign in to your account</p>
+                    <form action="" className="pt-15 flex gap-5 flex-col">
                         <div className="flex flex-col gap-2 ms-20 me-20 ">
                             <Label htmlFor="email">Email</Label>
                             <Input type="email" placeholder="Email" />
@@ -20,7 +20,7 @@ const PageLogin = () => {
                             <Input type="password" placeholder="Password" />
                         </div>
                         <Button className="w-110 mx-auto bg-blue-500 hover:bg-blue-600 cursor-pointer">Login</Button>
-                        <p className="text-sm text-end me-20">dont have an account? <span><Link href="/register" className="text-blue-500 underline">register</Link></span></p>
+                        <p className="text-sm text-end me-20 -mt-3">dont have an account? <span><Link href="/register" className="text-blue-500 underline">register</Link></span></p>
                     </form>
                 </div>
             </div>
